9fcc3110b5a5d244e57cd30243767f84efc27a9a
Ninjaruss <russ081999@gmail.com>
2026-01-30 02:32:11 -0800

    Added sameSite none as deployments are on separate platforms.

--- server/src/modules/auth/auth-discord.controller.ts
+++ server/src/modules/auth/auth-discord.controller.ts
@@ -62,7 +62,7 @@ export class AuthDiscordController {
       res.cookie('refreshToken', loginResult.refresh_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       });
     }
@@ -103,7 +103,7 @@ export class AuthDiscordController {
       res.cookie('refreshToken', loginResult.refresh_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       });
     }
--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -117,7 +117,7 @@ export class AuthController {
         res.cookie('refreshToken', payload.refresh_token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'lax',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         });
       }
